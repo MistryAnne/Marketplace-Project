a7d0dd25cf9db3bb0a35e2fcd4b05ee905f020e9
Anne Mistry <amistr34@my.centennialcollege.ca>
2023-10-17 13:49:42 -0400

    almost done testing rest, have trouble with search

--- config/config.mjs
+++ config/config.mjs
@@ -1,8 +1,9 @@
+
 const config = {
     env: process.env.NODE_ENV || 'development', 
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI || 
+    mongoUri: process.env.MONGODB_URI || "mongodb+srv://amistr34:O6sIsPPaaQFosgJr@cluster0.onk5dpb.mongodb.net/Marketplace?retryWrites=true&w=majority"||
     process.env.MONGO_HOST ||
     'mongodb://' + (process.env.IP || 'localhost') + ':' + 
     (process.env.MONGO_PORT || '27017') +
